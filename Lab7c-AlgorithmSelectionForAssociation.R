4ab21335ac7dcf83a01cb2690aa47af9c0c3f183
Course Files <137632706+course-files@users.noreply.github.com>
2023-10-15 18:24:13 +0300

    Created new file for 7c

---
+++ Lab7c-AlgorithmSelectionForAssociation.R
@@ -0,0 +1,54 @@
+# **Required Lab Work Submission** ----
+## Part A ----
+# Create a new file called
+# "Lab7-Submission-AlgorithmSelection.R".
+# Provide all the code you have used to demonstrate the training of a model.
+# This can be a model for a classification, regression, clustering, or an
+# association problem.
+# Each group member should provide one dataset that can be used for this
+# task as long as it is not one of the datasets we have used in Lab 7.
+
+## Part B ----
+# Upload *the link* to your
+# "Lab7-Submission-AlgorithmSelection.R" hosted
+# on Github (do not upload the .R file itself) through the submission link
+# provided on eLearning.
+
+## Part C ----
+# Create a markdown file called "Lab-Submission-Markdown.Rmd"
+# and place it inside the folder called "markdown". Use R Studio to ensure the
+# .Rmd file is based on the "GitHub Document (Markdown)" template when it is
+# being created.
+
+# Refer to the following file in Lab 1 for an example of a .Rmd file based on
+# the "GitHub Document (Markdown)" template:
+#     https://github.com/course-files/BBT4206-R-Lab1of15-LoadingDatasets/blob/main/markdown/BIProject-Template.Rmd # nolint
+
+# Include Line 1 to 14 of BIProject-Template.Rmd in your .Rmd file to make it
+# displayable on GitHub when rendered into its .md version
+
+# It should have code chunks that explain all the steps performed on the
+# datasets.
+
+## Part D ----
+# Render the .Rmd (R markdown) file into its .md (markdown) version by using
+# knitR in RStudio.
+
+# You need to download and install "pandoc" to render the R markdown.
+# Pandoc is a file converter that can be used to convert the following files:
+#   https://pandoc.org/diagram.svgz?v=20230831075849
+
+# Documentation:
+#   https://pandoc.org/installing.html and
+#   https://github.com/REditorSupport/vscode-R/wiki/R-Markdown
+
+# By default, Rmd files are open as Markdown documents. To enable R Markdown
+# features, you need to associate *.Rmd files with rmd language.
+# Add an entry Item "*.Rmd" and Value "rmd" in the VS Code settings,
+# "File Association" option.
+
+# Documentation of knitR: https://www.rdocumentation.org/packages/knitr/
+
+# Upload *the link* to "Lab-Submission-Markdown.md" (not .Rmd)
+# markdown file hosted on Github (do not upload the .Rmd or .md markdown files)
+# through the submission link provided on eLearning.
